b006abc0d92b06d338175d4e59eb0bcbea6c0209
AdityaGonnade <aditya.gonnade@swimlane.com>
2023-09-22 13:22:30 +0530

    initial backend and frontend login and signup

---
+++ Frontend/src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+  
+  public users: any =[];
+  constructor(
+    private auth : AuthService,
+    private api: ApiService) {
+  }
+
+  ngOnInit(){
+    this.api.getUsers()
+    .subscribe(res=>{
+      this.users = res;
+    })
+  }
+  
+  logout(){
+    this.auth.signOut();
+  }
+}
